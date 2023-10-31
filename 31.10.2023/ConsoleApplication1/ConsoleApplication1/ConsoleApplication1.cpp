45ffd599518e0013137bcecc9aad5ac324737572
pawelce <szkolacecot@gmail.com>
2023-10-31 10:26:44 +0100

    123smegma

---
+++ 31.10.2023/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    while (i <= 10){
+        if (i == 1) cout.width(5);
+        int j = 1;
+        while (j <= 10){
+            cout.width(5); cout << i * j;
+            j++;
+        } 
+        cout.width(5); cout << endl;
+        i++;
+    } 
+    /*int i = 1;
+    do
+    {
+        if (i == 1) cout.width(5);
+        int j = 1;
+        do {
+            cout.width(5); cout << i * j;
+            j++;
+        } while (j <= 10);
+        cout.width(5); cout << endl;
+        i++;
+        
+    } while (i <= 10);
+    */
+    /*
+    for (int i = 1; i <= 10; i++) 
+    {
+        if (i == 1) cout.width(5); //cout << i << " " << " ";
+        for (int j = 1; j <= 10; j++) {
+            cout.width(5); cout << i * j;
+        }
+        cout.width(5); cout << endl;
+    }
+    */
+    return 0;
+
+}
+
---
+++ 31.10.2023/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    fstream plik;
+    string linia;
+    int sumaP = 0;
+    int sumaNP = 0;
+    plik.open("dane.txt", ios::in);
+
+    if (plik.good() == true) {
+        cout << "MOM FILE CHOPIE" << endl;
+        while (getline(plik, linia))
+        {
+            if (stoi(linia) % 2 == 0) {
+                sumaP = sumaP + stoi(linia);
+            }
+            else {
+                sumaNP = sumaNP + stoi(linia); 
+
+            }
+        }
+            cout << "suma liczb parzystych = " << sumaP << endl;
+            cout << "suma liczb nieparzystych = " << sumaNP << endl;
+
+        
+    }
+    else {
+        cout << " NI MOM FILE CHOPIE" << endl;
+    }
+
+    plik.close();
+
+    return 0;
+}
+
